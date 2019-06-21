964e15782db4f8b5fddb7ae4cd0e7e673efd4cce
Argent77 <4519923+Argent77@users.noreply.github.com>
2019-06-21 10:07:47 +0200

    Cosmetic

--- tile2ee/Tile2EE.cpp
+++ tile2ee/Tile2EE.cpp
@@ -1,22 +1,3 @@
-/*
- * Copyright (C) 2012   argent77
- *
- * This file is part of tile2ee.
- *
- * tile2ee is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * tile2ee is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with tile2ee.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include <cstring>
 #include "Funcs.hpp"
 #include "Graphics.hpp"
